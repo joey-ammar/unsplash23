b2a6c420526879f47d6d089169e817aba07d3617
joey-ammar <xyx8703@gmail.com>
2023-11-27 12:56:55 +0100

    changed another file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import ThemeToggle from "./ThemeToggle";
 import SearchForm from "./SearchForm";
 import Gallery from "./Gallery";
-import { useGlobalContext } from "./Context";
+import { useGlobalContext } from "./context";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 const App = () => {
